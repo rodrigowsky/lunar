6e69ab9275dc11960be5c1d6f139b5eb59835add
Rodrigowsky <rmelo.res@gmail.com>
2023-02-15 22:57:32 -0100

    initial commit - project barebones boilerplate

---
+++ src/test/java/com/rodrigowsky/lunar/LunarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigowsky.lunar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
